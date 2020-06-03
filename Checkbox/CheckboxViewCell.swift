98b30eb6464d8e1637b1a8ebbe33b9b00c9f42ff
SashaShch <such.shch93@gmail.com>
2020-06-03 11:44:36 +0300

    first commit

---
+++ Checkbox/CheckboxViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CheckboxViewCell.swift
+//  Checkbox
+//
+//  Created by Рома on 02.06.2020.
+//  Copyright © 2020 SashaShch. All rights reserved.
+//
+
+import UIKit
+
+class CheckboxViewCell: UICollectionViewCell {
+    
+    var check = false
+    
+    @IBOutlet weak var checkImage: UIImageView!
+
+    
+}
--- Checkbox/ViewController.swift
+++ Checkbox/ViewController.swift
@@ -9,12 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let colors: [UIColor] = [.red, .green, .blue, .orange, .purple, .systemPink, .systemGray, .systemYellow, .red, .green, .blue, .orange, .purple, .systemPink, .systemGray, .systemYellow, .red, .green]
+    let cellCountPerRow = 3
+    let cellMargin: CGFloat = 7
+
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "box", for: indexPath) as! CheckboxViewCell
+        cell.backgroundColor = colors[indexPath.item]
+        
+        if cell.check {
+            cell.checkImage.isHidden = false
+        } else {
+            cell.checkImage.isHidden = true
+        }
+        
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! CheckboxViewCell
+        cell.check = !cell.check
+    }
+}
 
 
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets
+    {
+        return UIEdgeInsets(top: cellMargin, left: cellMargin, bottom: cellMargin, right: cellMargin)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = floor((UIScreen.main.bounds.width - CGFloat(cellCountPerRow + 1) * cellMargin) / CGFloat(cellCountPerRow))
+        
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return cellMargin
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return cellMargin
+    }
 }
 
